e18400ba0d77bd24e6c513d8b8e0005593d0532a
ramaraoyerramsetti <ram.yerram99@gmail.com>
2013-04-07 00:49:21 +0530

    commit message

---
+++ src/main/java/com/acc/FirstClass.java
@@ -0,0 +1,15 @@
+package com.acc;
+
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		System.out.println("This is  my first svn project....");
+		System.out.println("This is the second commit....");
+	}
+
+}
